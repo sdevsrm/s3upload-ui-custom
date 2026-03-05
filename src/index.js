538fded0c9ce1cf9385936fbf4332e005fe015fa
sdevsrm <srirajam@amazon.com>
2026-03-05 12:35:39 -0600

    Fix: use dynamic import to guarantee configure runs first

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
-// Configure BEFORE rendering App (which imports StorageBrowser)
+// Configure Amplify FIRST
 Amplify.configure(awsconfig);
 
-// Dynamic import ensures Amplify is configured before StorageBrowser loads
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// THEN dynamically import App (which imports StorageBrowser)
+import('./App').then(({ default: App }) => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
